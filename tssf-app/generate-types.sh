ebf872cc69193a6d7e2f2773b79464aec9c6d13b
Max Cantor <max@atrium.co>
2018-01-17 13:13:43 -0800

    Part 4 adding props to our component

--- tssf-app/generate-types.sh
+++ tssf-app/generate-types.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+set -e
 ENDPOINT="https://api.graph.cool/simple/v1/cjcjfm05056cp0141vrg7dpx0"
 GEN_DIR="src/gen/"
 SCHEMA="${GEN_DIR}/schema.json"
@@ -7,4 +9,9 @@ echo "export default '$ENDPOINT';" > "${GEN_DIR}/endpoint.ts"
 
 
 apollo-codegen introspect-schema "$ENDPOINT" --output "$SCHEMA"
+
+# Note that apollo-codegen tends to fail siliently.
+# If you get "...is not a module" errors, that may be because apollo-codegen failed
 apollo-codegen generate **/*.tsx --schema "$SCHEMA" --target typescript --output "${GEN_DIR}/gql-types.ts"
+
+echo "All Done"
