9dbfe24901c24f2dbab207f0ca0a1b7d8a409e76
Max Cantor <max@atrium.co>
2018-01-17 16:38:46 -0800

    Part 7 cosmedic

--- tssf-app/src/App.tsx
+++ tssf-app/src/App.tsx
@@ -34,7 +34,7 @@ class App extends React.Component<AppProps, AppState> {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to Typescript SF</h1>
           </header>
           <div className="main">
               Author Filter:
